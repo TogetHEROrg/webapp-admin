e2ae01651eddf7714ce6247e6de109010f2216f6
Marcela <marcela.desarrollosweb@gmail.com>
2019-09-18 14:15:44 -0300

    agregue los input y aparecen en el formulario , ok

--- src/Formulario/index.js
+++ src/Formulario/index.js
@@ -5,13 +5,10 @@ import './styles.css'
 export default class Formulario extends React.Component{
     state ={
         name:"",
-        apellido:"",
+        last_name:"",
         dni:"",
         direccion:"",
-        email:"",
-        celular:"",
-        organizacion:""
-
+        email:""
     };
 
 render(){
@@ -22,21 +19,41 @@ render(){
     <form className="col s12">
       <div className="row">
         <div className="input-field col s6">
-          <input placeholder="Placeholder" id="first_name" type="text" className="validate"/>
+          <input placeholder="Nombre" id="first_name" type="text" className="validate"/>
           <label htmlFor="name">Nombre:</label>
         </div>
 
         <div className="input-field col s6">
-          <input id="last_name" type="text" className="validate"/>
+          <input placeholder="Apellido" id="last_name" type="text" className="validate"/>
           <label htmlFor="last_name">Apellido:</label>
+        </div>
+      </div>
+      <div className="row">
+        <div className="input-field col s6">
+          <input placeholder="DNI" id="dni" type="text" className="validate"/>
+          <label htmlFor="dni">D.N.I:</label>
+        </div>
+
+        <div className="input-field col s6">
+          <input placeholder="Direccion" id="direccion" type="text" className="validate"/>
+          <label htmlFor="direccion">Direccion:</label>
         </div>
       </div>
         <div className="row">
         <div className="input-field col s12">
-          <input id="email" type="email" className="validate"/>
+          <input placeholder="Email" id="email" type="email" className="validate"/>
           <label htmlFor="email">Email</label>
         </div>
       </div>
+      <div class="input-field col s12">
+    <select>
+      <option value="" disabled selected>Elige una opcion:</option>
+      <option value="1">Option 1</option>
+      <option value="2">Option 2</option>
+      <option value="3">Option 3</option>
+    </select>
+    <label>Materialize Select</label>
+  </div>
       <input type="button" className="btn" onClick={this.onSubmit} value="Agregar"/>
         <input type="button" className="btn" onClick={()=>this.props.onCerrar()} value="Cerrar"/>
     </form>
