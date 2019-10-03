e4410cb19cdbcc400624e95907c51c6078d79677
Marcela <marcela.desarrollosweb@gmail.com>
2019-10-02 23:45:20 -0300

    formulario responsive

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -3,7 +3,6 @@ import './styles.css'
 import M from "materialize-css";
 import 'materialize-css/dist/css/materialize.min.css';
 
-
 export default class Formulario extends React.Component {
 
   componentDidMount() {
@@ -26,50 +25,55 @@ export default class Formulario extends React.Component {
   render() {
     return (
       <div className="row">
-        <div className="form-container col s9 z-depth-4 lime lighten-3">
-          <form className="col s12 hoverable">
-            <div className="row">
-              <div className="input-field col s6">
-                <i className="material-icons prefix">account_circle</i>
-                <input id="nombre" type="text" maxLength="25" className="validate" />
-                <label className="blue-text text-darken-2" htmlFor="nombre">Nombre:</label>
-              </div>
+      <div className="form-container col s12 lime lighten-3">
+      
 
-              <div className="input-field col s6">
-                <i className="material-icons prefix">account_box</i>
-                <input id="apellido" type="text" maxLength="25" className="validate" />
-                <label className="blue-text text-darken-2" htmlFor="apellido">Apellido:</label>
-              </div>
-            </div>
-            <div className="row">
-              <div className="input-field col s6">
-                <i className="material-icons prefix">recent_actors</i>
-                <input id="dni" type="text" maxLength="8" className="validate" />
-                <label className="blue-text text-darken-2" htmlFor="dni">D.N.I:</label>
-              </div>
+      <form className="form-style card-panel hoverable z-depth-5 col s12">
+      <div className="col s12">
+        
+      <div className="nav-wrapper z-depth-3">Nuevo Usuario:
+      <i class="material-icons">close</i>
+      </div>
+        
+        </div>
+      <div className="row">
+        <div className="input-field col s12 m6">
+        <i className="material-icons prefix">account_circle</i>
+        <input id="nombre" type="text" maxLength="25" className="validate" />
+        <label className="blue-text text-darken-2" htmlFor="nombre">Nombre:</label>
+        </div>
+        <div className="input-field col s12 m6">
+        <i className="material-icons prefix">account_box</i>
+        <input id="apellido" type="text" maxLength="25" className="validate" />
+        <label className="blue-text text-darken-2" htmlFor="apellido">Apellido:</label>
+        </div>
 
-              <div className="input-field col s6">
-                <i className="material-icons prefix">location_on</i>
-                <input id="direccion" type="text" maxLength="45" className="validate" />
-                <label className="blue-text text-darken-2" htmlFor="direccion">Direccion:</label>
-              </div>
-            </div>
-            <div className="row">
-              <div className="input-field col s6">
-                <i className="material-icons prefix">email</i>
-                <input id="email" type="email" maxLength="40" className="validate" />
-                <label className="blue-text text-darken-2" htmlFor="email">Email</label>
-                <span className="helper-text" data-error="Error" data-success="Correcto"></span>
-              </div>
-              <div className="input-field col s6">
-                <i className="material-icons prefix">phone</i>
-                <input id="telefono" type="tel" maxLength="30" className="validate" />
-                <label className="blue-text text-darken-2" htmlFor="telefono">Telefono:</label>
-                <span className="helper-text" data-error="Error" data-success="Correcto"></span>
-              </div>
-            </div>
-            <div className="row">
-              <div className="input-field col s4">
+        <div className="input-field col s12 m6">
+        <i className="material-icons prefix">recent_actors</i>
+        <input id="dni" type="text" maxLength="8" className="validate" />
+        <label className="blue-text text-darken-2" htmlFor="dni">D.N.I:</label>
+        </div>
+
+       <div className="input-field col s12 m6">
+       <i className="material-icons prefix">location_on</i>
+       <input id="direccion" type="text" maxLength="45" className="validate" />
+       <label className="blue-text text-darken-2" htmlFor="direccion">Direccion:</label>
+       </div>
+       <div className="input-field col s12 m6">
+      <i className="material-icons prefix">email</i>
+      <input id="email" type="email" maxLength="40" className="validate" />
+      <label className="blue-text text-darken-2" htmlFor="email">Email</label>
+      <span className="helper-text" data-error="Error" data-success="Correcto"></span>
+      </div>
+
+      <div className="input-field col s12 m6">
+      <i className="material-icons prefix">phone</i>
+      <input id="telefono" type="tel" maxLength="30" className="validate" />
+      <label className="blue-text text-darken-2" htmlFor="telefono">Telefono:</label>
+      <span className="helper-text" data-error="Error" data-success="Correcto"></span>
+      </div>
+
+      <div className="input-field col s12 m6">
                 <i className="material-icons prefix">group</i>
                 <select>
                   <option value="" disabled selected>Elige una organizacion:</option>
@@ -78,7 +82,7 @@ export default class Formulario extends React.Component {
                 </select>
               </div>
 
-              <div className="btn-check-posicion input-field col s4">
+              <div className="btn-check-posicion input-field col s12 m6">
                 <span>
                   <label>
                     <input name="group1" type="radio" checked />
@@ -93,7 +97,7 @@ export default class Formulario extends React.Component {
                 </span>
               </div>
 
-              <div className="btn-check-posicion col s4">
+              <div className="btn-check-posicion col s12 m6">
                 <button className="btn waves-effect waves-light" onClick={this.onSubmit} value="Agregar" type="submit" name="action">Agregar
               <i className="material-icons right">send</i>
                 </button>
@@ -101,10 +105,15 @@ export default class Formulario extends React.Component {
               <i className="material-icons right">close</i>
                 </button>
               </div>
-            </div>
-          </form>
-        </div>
+
       </div>
+      
+      
+      
+      </form>
+      </div>
+    </div> 
     );
   }
 }
+
